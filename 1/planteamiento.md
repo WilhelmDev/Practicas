934445576222f454b24aef1261a938c54ddf0382
Wilhelm Flores <wiljoseflores@gmail.com>
2024-06-18 08:20:47 -0400

    feat: first problem

---
+++ 1/planteamiento.md
@@ -0,0 +1,9 @@
+# Planteamiento
+
+Tenemos un array de objetos con X  cantidad de elementos y tenemos un segundo array que tiene algunos de los objetos del primer array.
+
+## ¿Qué se necesita hacer?
+
+Se requiere que se eliminen los del primer array, los objetos que están repetidos en el segundo, esto se debe hacer con las funciones nativas de js.
+
+Una vez hecho esto, requerimos saber cuantos elementos hay en cada array
---
+++ datos.js
@@ -0,0 +1,165 @@
+const array1 = [
+  {
+    "id": 1,
+    "name": "Select Sake Honjoso",
+    "slug": "select-sake-honjoso",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/HANAICHI/insignias/01-Insignia-Select-Sake-Honjoso.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  },
+  {
+    "id": 7,
+    "name": "Select Sake Nigori Snow",
+    "slug": "select-sake-nigori-snow",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/HANAICHI/insignias/02-Insignia-Select-Sake-Nigori-Snow.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  },
+  {
+    "id": 8,
+    "name": "Select Yuzu Citrico",
+    "slug": "select-yuzu-citrico",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/HANAICHI/insignias/03-Insignia-Select-Yuzu-Citrico.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  },
+  {
+    "id": 9,
+    "name": "Arte Cocina Japonesa",
+    "slug": "arte-cocina-japonesa",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/HANAICHI/insignias/04-Insignia-Arte-Cocina-Japonesa.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  },
+  {
+    "id": 10,
+    "name": "Select Ron Thai",
+    "slug": "Select-Ron-Thai",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/MAR_DEL_ZUR/insignias/01-Insignia-Select-Ron-Thai.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  },
+  {
+    "id": 11,
+    "name": "Select Whisky Thai",
+    "slug": "select-whisky-thai",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/MAR_DEL_ZUR/insignias/02-Insignia-Select-Whisky-Thai.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  },
+  {
+    "id": 12,
+    "name": "Natural Fusion Thai",
+    "slug": "natural-fusion-thai",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/MAR_DEL_ZUR/insignias/03-Insignia-Natural-Fusion-Thai.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  },
+  {
+    "id": 13,
+    "name": "Arte Cocina Thailandes",
+    "slug": "arte-cocina-thailandes",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/MAR_DEL_ZUR/insignias/04-Insignia-Arte-Cocina-Thailandes.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  },
+  {
+    "id": 14,
+    "name": "Sinfonia Tinto Cabernet Merlot",
+    "slug": "sinfonia-tinto-cabernet-merlot",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/SIBAU/insignias/01-Insignia-Sinfonia-Tinto-Cabernet-Merlot.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  },
+  {
+    "id": 15,
+    "name": "Sinfonia Blanco Sauvignon Blanc",
+    "slug": "sinfonia-blanco-sauvignon-blanc",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/SIBAU/insignias/02-Insignia-Sinfonia-Blanco-Sauvignon-Blanc.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  },
+  {
+    "id": 16,
+    "name": "Sinfornia Espumoso Champagne",
+    "slug": "sinfornia-espumoso-champagne",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/SIBAU/insignias/03-Insignia-Sinfornia-Espumoso-Champagne.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  },
+  {
+    "id": 18,
+    "name": "Arte Cocina Fusion",
+    "slug": "arte-cocina-fusion",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/SIBAU/insignias/04-Insignia-Arte-Cocina-Fusion.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  },
+  {
+    "id": 21,
+    "name": "Select Coñac",
+    "slug": "select-conac",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/insignias/01-Insignia-Select-Conac-Vainilla-Pistache.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  },
+  {
+    "id": 22,
+    "name": "Select Ron",
+    "slug": "select-ron",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/insignias/02-Insignia-Select-Ron-Tinta-Tabaco.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  },
+  {
+    "id": 23,
+    "name": "Select Vermut",
+    "slug": "select-vermut",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/insignias/03-Insignia-Select-Vermut-Coco.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  },
+  {
+    "id": 24,
+    "name": "Select Prensa Francesa",
+    "slug": "select-prensa-fran",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/insignias/04-Insignia-Select-Prensa-Francesa.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  }
+]
+
+const array2 = [
+  {
+    "id": 24,
+    "name": "Select Prensa Francesa",
+    "slug": "select-prensa-fran",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/insignias/04-Insignia-Select-Prensa-Francesa.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  },
+  {
+    "id": 21,
+    "name": "Select Coñac",
+    "slug": "select-conac",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/insignias/01-Insignia-Select-Conac-Vainilla-Pistache.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  },
+  {
+    "id": 14,
+    "name": "Sinfonia Tinto Cabernet Merlot",
+    "slug": "sinfonia-tinto-cabernet-merlot",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/SIBAU/insignias/01-Insignia-Sinfonia-Tinto-Cabernet-Merlot.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  },
+  {
+    "id": 7,
+    "name": "Select Sake Nigori Snow",
+    "slug": "select-sake-nigori-snow",
+    "imageName": "https://sibaritta.nyc3.cdn.digitaloceanspaces.com/PRODUCTION/HANAICHI/insignias/02-Insignia-Select-Sake-Nigori-Snow.png",
+    "createdAt": "2024-05-14T02:09:51.000Z",
+    "updatedAt": "2024-05-14T02:09:51.000Z"
+  }
+]
